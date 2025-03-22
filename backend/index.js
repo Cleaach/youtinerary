9de06a6fc7741b55b2f14c47abde96431bb268ed
Clement <clementaditya@gmail.com>
2025-03-22 14:37:25 +0800

    Fix environment variables

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,13 @@ const { getFirestore, collection, doc, deleteDoc, setDoc, getDoc, getDocs } = re
 
 // Firebase Config (Replace with your own)
 const firebaseConfig = {
-    apiKey: "AIzaSyBsVbWtVxWKY-fkZu426wd0HllvoZkbFjA",
-    authDomain: "youtinerary-6e346.firebaseapp.com",
-    projectId: "youtinerary-6e346",
-    storageBucket: "youtinerary-6e346.firebasestorage.app",
-    messagingSenderId: "418578080264",
-    appId: "1:418578080264:web:cca9237658f0774e8da7bd",
-    measurementId: "G-3YK8B883QJ"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
   };
 
 // Initialize Firebase
