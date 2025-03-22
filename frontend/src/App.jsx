190461607aae14b7ad7188ddff25af4cf60881fc
CelineTj16 <josephinecelinetj@gmail.com>
2025-03-22 16:57:40 +0800

    frontend 2git add .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home.jsx";
 
 // import CreateTripPage from "./pages/CreateTripPage";
-// import SignInPage from "./pages/SignInPage";
+import SignInPage from "./Pages/SignInPage";
 // import CommunityTripsPage from "./pages/CommunityTripsPage";
 
 function App() {
@@ -15,12 +15,9 @@ function App() {
       <Routes>
         {/* Landing Page at the root URL ("/") */}
         <Route path="/" element={<Home />} />
-
-        {/* Other pages (adjust these paths as needed) */}
-        {/* <Route path="/create-trip" element={<CreateTripPage />} />
+        {/* <Route path="/create-trip" element={<CreateTripPage />} /> */}
         <Route path="/signin" element={<SignInPage />} />
-        <Route path="/community-trips" element={<CommunityTripsPage />} /> */}
-
+        {/* <Route path="/community-trips" element={<CommunityTripsPage />} /> */}
         {/* Optional catch-all for 404, if you want */}
         {/* <Route path="*" element={<NotFoundPage />} /> */}
       </Routes>
